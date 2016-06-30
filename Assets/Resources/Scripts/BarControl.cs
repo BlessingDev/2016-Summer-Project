d0dbdeeb0eb39091eeeb855c5de21944bf7ab004
Junho Park <wnsghekd2006@naver.com>
2016-06-30 19:58:46 +0900

    Initial Commit

---
+++ Assets/Resources/Scripts/BarControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BarControl : MonoBehaviour {
+
+    [SerializeField]
+    private Image mRenderer = null;
+    [SerializeField]
+    private int mOriginalValue = 100;
+
+    private float mOriginalWidth = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (mRenderer == null)
+            this.enabled = false;
+        else
+        {
+            mOriginalWidth = mRenderer.sprite.rect.width;
+        }
+	}
+	
+    void SetValue(int val)
+    {
+        Vector3 scale = mRenderer.transform.localScale;
+
+        scale.x = val / mOriginalValue;
+
+        
+
+        mRenderer.transform.localScale = scale;
+    }
+}
