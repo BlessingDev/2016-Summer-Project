292bd65441ebf9cd41b3bf5d6f223e878018784e
Junho Park <wnsghekd2006@naver.com>
2016-06-30 20:39:09 +0900

    Add Bar Scripts

--- Assets/Resources/Scripts/BarControl.cs
+++ Assets/Resources/Scripts/BarControl.cs
@@ -5,31 +5,54 @@ using System.Collections;
 public class BarControl : MonoBehaviour {
 
     [SerializeField]
-    private Image mRenderer = null;
+    private Mask mMask = null;
     [SerializeField]
     private int mOriginalValue = 100;
+    [SerializeField]
+    private int mCurVal = 100;
 
-    private float mOriginalWidth = 0;
+    private bool mIsMoving = false;         // 현재 코루틴을 통한 바 갱신이 진행 중인가?
+    private float mSpeed = 0.3f;             // 바가 갱신되는 스피드
 
 	// Use this for initialization
 	void Start ()
     {
-        if (mRenderer == null)
+        if (mMask == null)
             this.enabled = false;
-        else
-        {
-            mOriginalWidth = mRenderer.sprite.rect.width;
-        }
 	}
 	
-    void SetValue(int val)
+    public void SetValue(int val)
     {
-        Vector3 scale = mRenderer.transform.localScale;
+        if(!mIsMoving)
+            StartCoroutine(UpdateBar(mCurVal, val));
+    }
 
-        scale.x = val / mOriginalValue;
+    private IEnumerator UpdateBar(int from, int to)
+    {
+        mIsMoving = true;
+        float val = mSpeed;
 
-        
+        if (from > to)
+            val *= -1;
 
-        mRenderer.transform.localScale = scale;
+        float targetScale = (float)to / mOriginalValue;
+
+        while(true)
+        {
+            Vector3 scale = mMask.transform.localScale;
+
+            scale.x += val * Time.smoothDeltaTime;
+
+            mMask.transform.localScale = scale;
+
+            bool endCheck = (val < 0) ? (scale.x < targetScale) : (scale.x > targetScale);
+            if(endCheck)
+            {
+                mIsMoving = false;
+                StopAllCoroutines();
+                break;
+            }
+            yield return null;
+        }
     }
 }
---
+++ Assets/Resources/Scripts/TestComp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestComp : MonoBehaviour
+{
+    [SerializeField]
+    private BarControl con = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (con != null)
+            con.SetValue(50);
+	}
+}
